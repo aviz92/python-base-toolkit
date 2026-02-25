b6194d26b30c9809e07de44f08c9bf4a0055c5a3
avizaguri-macbook <avi210892@gmail.com>
2026-02-25 08:28:09 +0200

    Add get_current_date_time_str function for formatted date-time output

--- python_base_toolkit/utils/date_time.py
+++ python_base_toolkit/utils/date_time.py
@@ -12,3 +12,8 @@ def datetime_now_with_timezone(timezone: ZoneInfo = utc_timezone) -> datetime:
     if timezone.key == "UTC":
         return now_utc
     return now_utc.astimezone(timezone)
+
+
+def get_current_date_time_str(timezone: ZoneInfo = utc_timezone, date_time_format: str = "%Y-%m-%d_%H%M%S") -> str:
+    now = datetime_now_with_timezone(timezone)
+    return now.strftime(date_time_format)
