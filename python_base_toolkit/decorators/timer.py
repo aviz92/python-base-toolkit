bd5b86a405dc73702f06421907b04811c36fb7ed
avizaguri-macbook <avi210892@gmail.com>
2026-05-14 13:01:44 +0300

    Enhance .gitignore and pre-commit configuration; update timer class for better logging

--- python_base_toolkit/decorators/timer.py
+++ python_base_toolkit/decorators/timer.py
@@ -9,27 +9,28 @@ logger = get_logger(__name__)
 
 
 class Timer:
-    def __init__(self) -> None:
+    def __init__(self, name: str | None = None) -> None:
+        self.name = name
         self.start_time = None
         self.end_time = None
         self.elapsed_time = None
 
     def __enter__(self) -> "Timer":
         self.start_time = time.perf_counter()
-        logger.info("Timer started.")
+        logger.info(f"Timer started for function: {self.name}")
         return self
 
     def __exit__(self, exc_type: type, exc_value: Exception, exc_traceback: object) -> None:
-        # logger.info(exc_type, exc_value, exc_traceback)
         self.end_time = time.perf_counter()
         self.elapsed_time = self.end_time - self.start_time
-        logger.info(f"Timer stopped. Elapsed time: {self.elapsed_time:.2f} seconds.")
+        logger.info(f"Timer stopped for function: {self.name}")
+        logger.info(f"Elapsed time for function: {self.name}: {self.elapsed_time}")
 
 
 def timer(func: Callable[..., Any]) -> Callable[..., Any]:
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        with Timer():
+        with Timer(func.__name__):
             return func(*args, **kwargs)
 
     return wrapper
